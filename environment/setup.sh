84fcd99dd529ae099609f64ffff9c76d91c2194b
Miguel Silva <a74601@alunos.uminho.pt>
2019-04-25 00:12:17 +0100

    add rdma libraries

--- environment/setup.sh
+++ environment/setup.sh
@@ -11,6 +11,5 @@ sudo apt-get install dist-upgrade
 sudo apt-get install build-essential git cmake pkg-config libnl-3-dev libnl-route-3-dev
 
 # RDMA libraries
-sudo apt-get install libibverbs1  libibverbs-dev  librdmacm-dev  librdmacm1  ibverbs-utils  rdmacm-utils
-
+sudo apt-get install libibverbs1  libibverbs-dev  librdmacm-dev  librdmacm1  ibverbs-utils  rdmacm-utils ibutils libibcommon1 ibdapl2 ibsim-utils libcxgb3-1 libibmad5 libibumad3 libmlx4-1 libmthca1 libnes1
 # After this, reboot!
