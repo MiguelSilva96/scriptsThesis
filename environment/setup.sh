1cf5dffd3db070c1a6b6a8806d81af790905244a
Miguel Silva <a74601@alunos.uminho.pt>
2019-04-24 23:24:34 +0100

    Use dist-upgrade to update kernel version safely

--- environment/setup.sh
+++ environment/setup.sh
@@ -4,6 +4,6 @@
 sudo apt-get update
 
 # Use a kernel version compatible with softiwarp
-sudo apt-get install linux-image-4.15.0-38-generic linux-headers-4.15.0-38-generic
+sudo apt-get install dist-upgrade
 
 # After this, reboot!
